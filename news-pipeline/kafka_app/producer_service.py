fd3a7e1bab063264fa4849f8f053f4cb32cd4841
Ernest Gaise <ernestyawgaisie@gmail.com>
2025-06-03 23:17:51 -0400

    (feat): Data cleaning in spark, duplicates check - Pipeline working complete

--- news-pipeline/kafka_app/producer_service.py
+++ news-pipeline/kafka_app/producer_service.py
@@ -1,6 +1,7 @@
 # kafka/producer.py
 
 from kafka import KafkaProducer
+from pymongo import MongoClient
 import json
 from scrapers.bbc_trend.bbc import scrape_bbc_full_articles
 from scrapers.techcrunch.techcrunch import scrape_multiple_articles
@@ -11,6 +12,9 @@ import os
 
 sys.path.append('/opt/airflow') 
 
+MONGO_URI = "mongodb+srv://ernestyawgaisie:ernestyawgaisie@cluster0.dvjsafm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(MONGO_URI)
+collection = client["newsdb"]["articles"]
 
 def send_articles_to_kafka():
     print("Scraping BBC articles...")
@@ -22,18 +26,24 @@ def send_articles_to_kafka():
         value_serializer=lambda v: json.dumps(v).encode("utf-8")
     )
 
+    sent_count = 0
     for article in articles:
+        if collection.find_one({"url": article["url"]}):
+            print(f"Duplicate skipped: {article['url']}")
+            continue
+
         try:
             producer.send(KAFKA_TOPIC, article)
             print(f"Sent to Kafka: {article['title']}")
+            sent_count += 1
         except Exception as e:
             print(f"Failed to send: {e}")
 
     producer.flush()
-    print("All BBC articles sent!")
+    print(f"{sent_count} articles sent to Kafka!")
 
     print("Scraping TechCrunch Articles.")
-    scrape_multiple_articles()
+    # scrape_multiple_articles()
     print("All TechCrunch articles sent!")
 
 
