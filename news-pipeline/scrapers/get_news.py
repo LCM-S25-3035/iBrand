3527220fe4ff5a8ffc42dd992c2c1d405be4c06c
Sarita Tanwar <Saritatanwar1997@gmail.com>
2025-07-14 10:30:05 -0400

    Update get_news.py

--- news-pipeline/scrapers/get_news.py
+++ news-pipeline/scrapers/get_news.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
-
 import os
 from dotenv import load_dotenv
 from news_fetcher import fetch_news
 from datetime import datetime, timedelta
+from pymongo import MongoClient
 
-load_dotenv()  # loads .env file in the same directory
+load_dotenv()
 
-API_KEY = os.getenv("NEWS_API_KEY")  # get key from environment variable
+API_KEY = os.getenv("NEWS_API_KEY")
+mongo_uri = os.getenv("MONGO_URI")
 
+# Mongo connection
+client = MongoClient(mongo_uri)
+db = client["iBrandDB"]
+collection = db["articles_last_30_days"]
+
+# Date range
 QUERY = "news"
 to_date = datetime.utcnow()
 from_date = to_date - timedelta(days=30)
 
+# Fetch news
 df = fetch_news(API_KEY, QUERY, from_date, to_date)
+
+# Save to MongoDB
+records = df.to_dict(orient="records")
+if records:
+    collection.insert_many(records)
+    print(f"✅ Inserted {len(records)} articles into MongoDB.")
+else:
+    print("⚠️ No articles found to insert.")
+
+# Optional: still save to CSV
 save_path = r"C:\Users\sarita\Downloads\newsapi_articles_last_month.csv"
 df.to_csv(save_path, index=False)
-
 print(f"✅ Done! {len(df)} articles saved to:\n{save_path}")
